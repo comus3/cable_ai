bc4430eb06880916ccecacfafd33f8a64c727336
Pierre Dederix <p_dederix@hotmail.com>
2024-04-08 15:56:40 +0200

    initialize method

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from torchvision.io import read_image
 import torchvision.transforms.functional as F
-device = torch.device('cuda')
+import torchvision.transforms as T
+from torchvision.utils import make_grid
+from utils import *
 
+
+
+
+def init():
+    global filePaths
+    global thisPath
+    thisPath = '/Users/lionelschinckus/Documents/cable_ai/git/cable_ai'
+    trainingFolderPath = '/Users/lionelschinckus/Documents/cable_ai/docs/trainingData/masks'
+    fileNames = get_jpg_filenames(trainingFolderPath)
+    filePaths = []
+    for fileName in fileNames:
+        filePaths.append(str(Path(trainingFolderPath) / fileName))
+
+
+    
+
+init()
